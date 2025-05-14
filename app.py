0b9890c6058d30843a33c39af3a9f419b3429879
sjbang <sjbang8098@gmail.com>
2025-05-14 17:19:32 +0900

    포트 수정

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def download_exe():
     return send_file("data/unlockSApp.exe", mimetype='application/octet-stream', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
